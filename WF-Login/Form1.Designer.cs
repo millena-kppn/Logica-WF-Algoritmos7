82e54981dee0bbff4c3b5aa1b7a21a0c646bd813
Millena Kappaun <kappaunmillena@gmail.com>
2025-04-10 21:59:43 -0300

    projeto feito

--- WF-Login/Form1.Designer.cs
+++ WF-Login/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WF_Login
 {
-    partial class Form1
+    partial class Formulario
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario));
+            panel1 = new Panel();
+            pictureBox3 = new PictureBox();
+            txtBoxOne = new TextBox();
+            txtBoxTwo = new TextBox();
+            btnVerificar = new Button();
+            linkLabel1 = new LinkLabel();
+            pictureBoxSair = new PictureBox();
+            pictureBoxMin = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSair).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMin).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Window;
+            panel1.Controls.Add(pictureBox3);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(288, 450);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(47, 157);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(174, 165);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // txtBoxOne
+            // 
+            txtBoxOne.ForeColor = SystemColors.AppWorkspace;
+            txtBoxOne.Location = new Point(360, 178);
+            txtBoxOne.Name = "txtBoxOne";
+            txtBoxOne.Size = new Size(353, 31);
+            txtBoxOne.TabIndex = 1;
+            txtBoxOne.Text = "Nome...";
+            txtBoxOne.Enter += txtBoxOne_Enter;
+            txtBoxOne.Leave += txtBoxOne_Leave;
+            // 
+            // txtBoxTwo
+            // 
+            txtBoxTwo.ForeColor = SystemColors.AppWorkspace;
+            txtBoxTwo.Location = new Point(360, 237);
+            txtBoxTwo.Name = "txtBoxTwo";
+            txtBoxTwo.Size = new Size(353, 31);
+            txtBoxTwo.TabIndex = 2;
+            txtBoxTwo.Text = "Senha...";
+            // 
+            // btnVerificar
+            // 
+            btnVerificar.Location = new Point(491, 288);
+            btnVerificar.Name = "btnVerificar";
+            btnVerificar.Size = new Size(112, 34);
+            btnVerificar.TabIndex = 3;
+            btnVerificar.Text = "Verificar";
+            btnVerificar.UseVisualStyleBackColor = true;
+            btnVerificar.Click += btnVerificar_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(456, 348);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(176, 25);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueci minha senha";
+            // 
+            // pictureBoxSair
+            // 
+            pictureBoxSair.Image = (Image)resources.GetObject("pictureBoxSair.Image");
+            pictureBoxSair.Location = new Point(766, 0);
+            pictureBoxSair.Name = "pictureBoxSair";
+            pictureBoxSair.Size = new Size(35, 31);
+            pictureBoxSair.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxSair.TabIndex = 5;
+            pictureBoxSair.TabStop = false;
+            pictureBoxSair.Click += pictureBoxSair_Click;
+            // 
+            // pictureBoxMin
+            // 
+            pictureBoxMin.Image = (Image)resources.GetObject("pictureBoxMin.Image");
+            pictureBoxMin.Location = new Point(744, 0);
+            pictureBoxMin.Name = "pictureBoxMin";
+            pictureBoxMin.Size = new Size(26, 31);
+            pictureBoxMin.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxMin.TabIndex = 6;
+            pictureBoxMin.TabStop = false;
+            pictureBoxMin.Click += pictureBoxMin_Click;
+            // 
+            // Formulario
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SkyBlue;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBoxMin);
+            Controls.Add(pictureBoxSair);
+            Controls.Add(linkLabel1);
+            Controls.Add(btnVerificar);
+            Controls.Add(txtBoxTwo);
+            Controls.Add(txtBoxOne);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Formulario";
+            Opacity = 0.95D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSair).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxMin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox txtBoxOne;
+        private TextBox txtBoxTwo;
+        private Button btnVerificar;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBoxSair;
+        private PictureBox pictureBoxMin;
     }
 }
--- WF-Login/Form1.cs
+++ WF-Login/Form1.cs
@@ -1,10 +1,42 @@
 namespace WF_Login
 {
-    public partial class Form1 : Form
+    public partial class Formulario : Form
     {
-        public Form1()
+        public Formulario()
         {
             InitializeComponent();
         }
+
+        private void pictureBoxSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            txtBoxOne.Text = "Nome cadastrado";
+            txtBoxOne.ForeColor = Color.Green;
+            txtBoxTwo.Text = "Senha aceita";
+            txtBoxTwo.ForeColor = Color.Green;
+        }
+
+        private void pictureBoxMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized; // Minimiza a janela
+        }
+
+        private void txtBoxOne_Enter(object sender, EventArgs e)
+        {
+            if (txtBoxOne.Text == "Nome...")
+                txtBoxOne.Text = "";
+        }
+
+        private void txtBoxOne_Leave(object sender, EventArgs e)
+        {
+            if (txtBoxOne.Text == "")
+                txtBoxOne.Text = "Nome...";
+        }
+
+        
     }
 }
